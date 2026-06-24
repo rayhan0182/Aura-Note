97221bfeaab2b6b0645b2690169ac5e75dfd969c
Rayhan <mdrayhan8045@gmail.com>
2026-06-24 12:45:37 +0600

    note_page_design1_initial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,11 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+
+    implementation("com.intuit.ssp:ssp-android:1.1.1")
+
+    implementation("com.intuit.sdp:sdp-android:1.1.1")
+
+
+
 }
